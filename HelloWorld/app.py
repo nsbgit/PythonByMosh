06d4015ccc06e0bac848157db57b418030849a24
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-04-14 01:15:21 +0530

    Variables

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,4 +1,10 @@
-print("Sukanta Sharma")
-print('0----')
-print(' ||||')
-print('*' * 10)
+price = 10
+rating = 4.9
+name = 'Sukanta'
+is_published = False
+#Use underscore(_) to seperate words in variable name
+#defining variables use lower case letter but 'Flase' here is a keyword
+print(price)
+print(rating)
+print(name)
+print(is_published)
