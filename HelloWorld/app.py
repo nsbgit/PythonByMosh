9d176c163a583d95d8d17a3a64858d6899ad9f54
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-04-14 01:38:47 +0530

    getting input excercize

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,3 +1,6 @@
 #Getting input
+# Ask two questions: person's name and favorite color.
+# Then, print a message like "Mosh likes blue"
 name = input('What is your name? ')
-print('Hi ' + name)
+fovorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + fovorite_color)
