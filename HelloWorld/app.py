f10f248e86efdf52e7b5fe4086d59c386b80f8a9
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-05-01 00:40:20 +0530

    Strings - Basic
    Signed-off-by: Sukanta Sharma <nsbgit@users.noreply.github.com>

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,7 +1,26 @@
 """
-Type Conversion Exercise :
-Ask a user their weight (in pounds), convert it to kilograms and print on the terminal.
+Strings
 """
-weigth_lbs = input('What is your weight (lbs)? ')
-weigth_kg = 0.45 * float(weigth_lbs)
-print('Your weight is ' + str(weigth_kg) +' kg')
+course = 'Python for Beginners'
+"""
+We can use ' or " for string but it is important to know where you can use what.
+If we want to use ' in between => use " for string
+If we want to use " in between => use ' for string
+
+String with Multi-line string use ''' or
+"""
+
+course1 = "Python's Course for Beginners"
+course2 = 'Python for "Beginners"'
+course3 = '''
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+'''
+print(course)
+print(course1)
+print(course2)
+print(course3)
