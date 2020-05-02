c12dad30ce894f72e1b627a83c9849f8cafc4572
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-05-02 16:45:40 +0530

    Arithmetic Operations
    Signed-off-by: Sukanta Sharma <nsbgit@users.noreply.github.com>

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,39 +1,16 @@
 # """
-# Strings Methods
+# Arithmetic Operations
 # """
 
-# We can do really cool things using string functions.
-# FUNCTION: block of code responsible for a set of work
-# METHOD: when function is associated with any object
-# len() is a function, in-contrast upper() is a method
+print(f'10 + 3 = {10 + 3} => Addition')  # 13
+print(f'10 - 3 = {10 - 3} => Subtraction')  # 7
+print(f'10 * 3 = {10 * 3} => Multiplication')  # 30
+print(f'10 / 3 = {10 / 3} => Division (returns floating point value)')  # 3.33...
+print(f'10 // 3 = {10 // 3} => Division (returns floor int value). This is exclusive in Python')  # 3
+print(f'10 % 3 = {10 % 3} => Modulus (returns remainder of division)')  # 1
+print(f'10 ** 3 = {10 ** 3} => Power (returns 10 to the power 3)')  # 1000
 
-course = 'Python for Beginners course in on'
-#         0123456789...
-
-# BASIC FUNCTIONS AND METHODS:
-print(f'len() => {len(course)}')
-print(f'upper() = {course.upper()}')
-print(f'lower() => {course.lower()}')
-print(f'title() => {course.title()}')  # Returns a string which has first letter in each word as CAPITAL and the rest
-# are as small
-print(f'Original String => {course}')
-
-# FIND METHOD:
-print(course.find('n'))  # returns starting index of first occurrence of character(s){CASE_SENSITIVE} => 5
-print(course.find('on'))  # 4
-print(course.find('z'))  # returns -1 if the character(s) is/are not found => -1
-print(course.find('fg'))  # -1
-
-# REPLACE METHOD:
-print(course.replace('Beginners', 'Absolute Beginner'))
-print(course.replace('P', 'J'))
-print(course.replace('xyz', 'abc'))  # if the searched string is not found it would return the original string
-
-# IN OPERATOR:
-# in operator is used to search character(s) in a given string. It returns boolean value,
-# whereas find() returns starting index of character(s).
-print('Python' in course)  # returns True
-print('Sukanta' in course)  # returns False
-
-
-
+# AUGMENTED ASSIGNMENT OPERATOR:
+x = 10
+x += 3  # It is similar to x = x + 3. It is applicable to all arithmetic operators
+print(x)
