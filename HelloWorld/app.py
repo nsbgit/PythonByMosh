3245c25c78a33bec04d41c0120356eef992bb22c
Sukanta Sharma <nsbgit@users.noreply.github.com>
2020-05-04 00:08:51 +0530

    Operator Precedence
    Signed-off-by: Sukanta Sharma <nsbgit@users.noreply.github.com>

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,16 +1,13 @@
 # """
-# Arithmetic Operations
+# Operator Precedence
 # """
 
-print(f'10 + 3 = {10 + 3} => Addition')  # 13
-print(f'10 - 3 = {10 - 3} => Subtraction')  # 7
-print(f'10 * 3 = {10 * 3} => Multiplication')  # 30
-print(f'10 / 3 = {10 / 3} => Division (returns floating point value)')  # 3.33...
-print(f'10 // 3 = {10 // 3} => Division (returns floor int value). This is exclusive in Python')  # 3
-print(f'10 % 3 = {10 % 3} => Modulus (returns remainder of division)')  # 1
-print(f'10 ** 3 = {10 ** 3} => Power (returns 10 to the power 3)')  # 1000
+# Exponentiation
+# Multiplication or Division
+# Addition or Subtraction
 
-# AUGMENTED ASSIGNMENT OPERATOR:
-x = 10
-x += 3  # It is similar to x = x + 3. It is applicable to all arithmetic operators
-print(x)
+# For more information about operator precedence visit
+# https://www.tutorialspoint.com/python/operators_precedence_example.htm
+
+print(f'10 + 3 * 2 = {10 + 3 * 2}')
+print(f'(10 + 3) * 2 = {(10 + 3) * 2}')
